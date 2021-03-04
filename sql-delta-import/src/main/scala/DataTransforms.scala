1ff528bea2bae0515ea5de9b71c53675fc04ef24
Alex Kushnir <alexkushnir@scribd.com>
2021-03-04 16:18:18 -0500

    changing copyright to Scribd

--- sql-delta-import/src/main/scala/DataTransforms.scala
+++ sql-delta-import/src/main/scala/DataTransforms.scala
@@ -1,5 +1,6 @@
+// scalastyle:off
 /*
- * Copyright (2020) The Delta Lake Project Authors.
+ * Copyright (2021) Scribd Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// scalastyle:on
 
 package io.delta.connectors.spark.JDBC
 
