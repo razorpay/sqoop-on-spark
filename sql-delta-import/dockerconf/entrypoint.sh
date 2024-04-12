15ccc7b6ca490a232e9fc9106230aad9320a8a44
rajkaran <rajkaran.sharma@razorpay.com>
2024-04-12 19:14:51 +0530

    cleanup

--- sql-delta-import/dockerconf/entrypoint.sh
+++ sql-delta-import/dockerconf/entrypoint.sh
@@ -6,7 +6,7 @@ cd /app
 upload_jar()
 {
     local_jar_path="sql-delta-import/target/scala-2.12/sql-delta-import-assembly-0.2.1-SNAPSHOT.jar"
-    jar_s3_url="s3://rzp-edh-dev/application-jars"
+    jar_s3_url="s3://${SQOOP_BUCKET}/application-jars"
     full_jar_s3_url="${jar_s3_url}/sqoop-1.0-SNAPSHOT-jar-with-dependencies.jar"
 
     aws s3 cp --acl "bucket-owner-full-control" ${local_jar_path} ${full_jar_s3_url}
